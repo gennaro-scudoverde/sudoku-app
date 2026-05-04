2b9a1597f329aa7b14ae518259dcb8204c023770
gennaro-scudoverde <gennarocampana@gmail.com>
2026-05-04 14:03:08 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,23 @@
-const CACHE_NAME = "sudoku-app-v2";
-
-
-const urlsToCache = [
-  "/sudoku-app/",
-  "/sudoku-app/index.html",
-  "/sudoku-app/manifest.json",
-  "/sudoku-app/icon-192x192.png",
-  "/sudoku-app/icon-512x512.png"
-];
-
+const CACHE_NAME = "sudoku-app-v3";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(names =>
-      Promise.all(
-        names.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192x192.png",
+        "./icon-512x512.png"
+      ]);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
